34abb8889b8618a36bfcdeff72376bd4f9c0a551
servosdesign <servos.design0@gmail.com>
2022-05-27 13:47:26 -0500

    nft-display components, styled-components import, asset naming correction

---
+++ frontend/src/components/nft-display/nft-display.styles.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+export const StyledDisplay = styled.div`
+  display: flex;
+  justify-content: center;
+  text-align: center;
+
+  &   img {
+    height: 500px;
+    width: 500px;
+  }
+
+  & p {
+    margin: 0 auto;
+  }
+`;
+
