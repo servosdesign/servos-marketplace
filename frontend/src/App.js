01b8f3a86142660362c17d76afdb1c96590032cc
servosdesign <servos.design0@gmail.com>
2022-05-28 19:04:23 -0500

    web3Auth connect button, config creation, login button connected to web3auth

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,14 @@
+import { useState } from 'react';
+
 import NftDisplay from './components/nft-display/nft-display.component';
+import LoginButton from './components/login-button/login-button.component';
 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <LoginButton />
       <NftDisplay />
     </div>
   );
---
+++ frontend/src/config/config.js
@@ -0,0 +1,11 @@
+export const network = {
+  displayName: "Ethereum Rinkeby",
+  chainNamespace: "eip155",
+  chainId: "0x4",
+  rpcTarget: `https://rinkeby.infura.io/v3/d59f442644c14d1e88dc58554ed5be6a`,
+  blockExplorer: "https://rinkeby.etherscan.io/",
+  ticker: "ETH",
+  tickerName: "Ethereum",
+}
+
+export const clientId = process.env.REACT_APP_WEB3_AUTH_CLIENT_ID;
